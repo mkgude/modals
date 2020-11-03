cfe3e7d50f46963973d5346a66fb630c77be48fb
Marian Gude <mkgude@gmail.com>
2020-11-03 13:55:36 -0600

    added modal functionality with click functionality

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const modal = document.querySelector(".modal")
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnShowModal = document.querySelectorAll(".show-modal")
+
+console.log(btnShowModal)
+
+for (let i = 0; i < btnShowModal.length; i++){
+    btnShowModal[i].addEventListener("click", function() {
+        console.log("Button Clicked");
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    })
+}
+btnCloseModal.addEventListener("click", function(){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+})
+
